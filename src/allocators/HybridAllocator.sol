2ac00e202147b4f441c2104fb4c828cfefc4fb18
mgretzke <m.gretzke@vimage.de>
2025-07-02 19:20:54 +0200

    Added HybridAllocator and HybridERC7683 allocator

---
+++ src/allocators/HybridAllocator.sol
@@ -0,0 +1,230 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import {BatchCompact, Lock} from '@uniswap/the-compact/types/EIP712Types.sol';
+
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {IAllocator} from '@uniswap/the-compact/interfaces/IAllocator.sol';
+import {ITheCompact} from '@uniswap/the-compact/interfaces/ITheCompact.sol';
+import {IERC7683Allocator} from 'src/interfaces/IERC7683Allocator.sol';
+
+contract HybridAllocator is IAllocator {
+    uint96 public immutable ALLOCATOR_ID;
+    ITheCompact private immutable _COMPACT;
+    bytes32 private immutable _COMPACT_DOMAIN_SEPARATOR;
+
+    mapping(bytes32 => bool) private claims;
+
+    uint256 public nonce;
+    uint256 public signerCount;
+    mapping(address => bool) public signers;
+
+    error Unsupported();
+    error InvalidIds();
+    error InvalidAllocatorId(uint96 allocatorId, uint96 expectedAllocatorId);
+    error InvalidCaller(address sender, address expectedSender);
+    error InvalidAllocatorData(uint256 length);
+    error InvalidSignature();
+    error InvalidSigner();
+    error LastSigner();
+    error InvalidValue(uint256 value, uint256 expectedValue);
+
+    event ClaimRegistered(address indexed sponsor, uint256[] registeredAmounts, uint256 nonce, bytes32 claimHash);
+
+    modifier onlySigner() {
+        if (!signers[msg.sender]) {
+            revert InvalidSigner();
+        }
+        _;
+    }
+
+    constructor(address compact_, address signer_) {
+        _COMPACT = ITheCompact(compact_);
+        ALLOCATOR_ID = _COMPACT.__registerAllocator(address(this), '');
+        _COMPACT_DOMAIN_SEPARATOR = _COMPACT.DOMAIN_SEPARATOR();
+
+        signers[signer_] = true;
+        signerCount++;
+
+        // Block the first half of nonces for the offchain allocator
+        nonce = type(uint128).max;
+    }
+
+    function addSigner(address signer_) external onlySigner {
+        signers[signer_] = true;
+        signerCount++;
+    }
+
+    function removeSigner(address signer_) external onlySigner {
+        if (signerCount == 1) {
+            revert LastSigner();
+        }
+        signers[signer_] = false;
+        signerCount--;
+    }
+
+    function replaceSigner(address newSigner_) external onlySigner {
+        signers[msg.sender] = false;
+        signers[newSigner_] = true;
+    }
+
+    function attest(address, /*operator*/ address, /*from*/ address, /*to*/ uint256, /*id*/ uint256 /*amount*/ )
+        external
+        pure
+        returns (bytes4)
+    {
+        revert Unsupported();
+    }
+
+    function registerClaim(
+        address recipient,
+        uint256[2][] memory idsAndAmounts,
+        address arbiter,
+        uint256 expires,
+        bytes32 typehash,
+        bytes32 witness
+    ) public payable returns (bytes32, uint256[] memory, uint256) {
+        idsAndAmounts = _actualIdsAndAmounts(idsAndAmounts);
+
+        (bytes32 claimHash, uint256[] memory registeredAmounts) = _COMPACT.batchDepositAndRegisterFor{value: msg.value}(
+            recipient, idsAndAmounts, arbiter, ++nonce, expires, typehash, witness
+        );
+
+        // Allocate the claim
+        claims[claimHash] = true;
+
+        emit ClaimRegistered(recipient, registeredAmounts, nonce, claimHash);
+
+        return (claimHash, registeredAmounts, nonce);
+    }
+
+    function authorizeClaim(
+        bytes32 claimHash,
+        address, /*arbiter*/
+        address, /*sponsor*/
+        uint256, /*nonce*/
+        uint256, /*expires*/
+        uint256[2][] calldata, /*idsAndAmounts*/
+        bytes calldata allocatorData_
+    ) external returns (bytes4) {
+        if (msg.sender != address(_COMPACT)) {
+            revert InvalidCaller(msg.sender, address(_COMPACT));
+        }
+
+        // Check if the claim was allocated on chain
+        if (claims[claimHash]) {
+            delete claims[claimHash];
+
+            // Authorize the claim
+            return IAllocator.authorizeClaim.selector;
+        }
+
+        // Check the allocator data for a valid signature by an authorized signer
+        bytes32 digest = keccak256(abi.encodePacked(bytes2(0x1901), _COMPACT_DOMAIN_SEPARATOR, claimHash));
+        if (!_checkAllocatorData(digest, allocatorData_)) {
+            revert InvalidSignature();
+        }
+
+        // Authorize the claim
+        return IAllocator.authorizeClaim.selector;
+    }
+
+    function isClaimAuthorized(
+        bytes32 claimHash, // The message hash representing the claim.
+        address, /*arbiter*/ // The account tasked with verifying and submitting the claim.
+        address, /*sponsor*/ // The account to source the tokens from.
+        uint256, /*nonce*/ // A parameter to enforce replay protection, scoped to allocator.
+        uint256, /*expires*/ // The time at which the claim expires.
+        uint256[2][] calldata, /*idsAndAmounts*/ // The allocated token IDs and amounts.
+        bytes calldata allocatorData // Arbitrary data provided by the arbiter.
+    ) external view returns (bool) {
+        if (claims[claimHash]) {
+            return true;
+        }
+
+        // Check the allocator data for a valid signature by an authorized allocator address
+        bytes32 digest = keccak256(abi.encodePacked(bytes2(0x1901), _COMPACT_DOMAIN_SEPARATOR, claimHash));
+        return _checkAllocatorData(digest, allocatorData);
+    }
+
+    function _splitId(uint256 id) internal pure returns (uint96 allocatorId_, address token_) {
+        return (_splitAllocatorId(id), _splitToken(id));
+    }
+
+    function _splitAllocatorId(uint256 id) internal pure returns (uint96 allocatorId_) {
+        assembly ("memory-safe") {
+            allocatorId_ := shr(164, shl(4, id))
+        }
+        return allocatorId_;
+    }
+
+    function _splitToken(uint256 id) internal pure returns (address) {
+        return address(uint160(id));
+    }
+
+    function _createLock(uint256 id, uint256 amount) internal pure returns (Lock memory) {
+        return Lock({lockTag: bytes12(bytes32(id)), token: _splitToken(id), amount: amount});
+    }
+
+    function _checkAllocatorData(bytes32 digest, bytes memory allocatorData_) private view returns (bool) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        if (allocatorData_.length == 65) {
+            (r, s) = abi.decode(allocatorData_, (bytes32, bytes32));
+            v = uint8(allocatorData_[64]);
+        } else if (allocatorData_.length == 64) {
+            bytes32 vs;
+            (r, vs) = abi.decode(allocatorData_, (bytes32, bytes32));
+            v = uint8(uint256(vs >> 255) + 27);
+            s = vs << 1 >> 1;
+        } else {
+            return false;
+        }
+
+        // Check if the signer is an authorized allocator address
+        return signers[ecrecover(digest, v, r, s)];
+    }
+
+    function _actualIdsAndAmounts(uint256[2][] memory idsAndAmounts) internal returns (uint256[2][] memory) {
+        uint256 idIndex = 0;
+        uint256 idsLength = idsAndAmounts.length;
+        if (idsLength == 0) {
+            revert InvalidIds();
+        }
+
+        // Check for native token - Native tokens must always be the first id
+        if (_splitToken(idsAndAmounts[0][0]) == address(0)) {
+            // Check allocator id
+            if (_splitAllocatorId(idsAndAmounts[0][0]) != ALLOCATOR_ID) {
+                revert InvalidAllocatorId(_splitAllocatorId(idsAndAmounts[0][0]), ALLOCATOR_ID);
+            }
+            if (idsAndAmounts[0][1] != 0 && msg.value != idsAndAmounts[0][1]) {
+                revert InvalidValue(msg.value, idsAndAmounts[0][1]);
+            }
+            idsAndAmounts[0][1] = msg.value;
+
+            idIndex++;
+        }
+
+        for (; idIndex < idsLength; idIndex++) {
+            (uint96 allocatorId_, address token_) = _splitId(idsAndAmounts[idIndex][0]);
+
+            // Check allocator id
+            if (allocatorId_ != ALLOCATOR_ID) {
+                revert InvalidAllocatorId(allocatorId_, ALLOCATOR_ID);
+            }
+
+            if (idsAndAmounts[idIndex][1] == 0) {
+                // Amount is derived from the allocators token balance
+                idsAndAmounts[idIndex][1] = IERC20(token_).balanceOf(address(this));
+            }
+
+            IERC20(token_).approve(address(_COMPACT), idsAndAmounts[idIndex][1]);
+        }
+
+        return idsAndAmounts;
+    }
+}
---
+++ src/allocators/HybridERC7683.sol
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import {LibBytes} from 'solady/utils/LibBytes.sol';
+
+import {BatchClaim, Mandate} from './types/TribunalStructs.sol';
+import {BatchCompact, Lock} from '@uniswap/the-compact/types/EIP712Types.sol';
+
+import {HybridAllocator} from 'src/allocators/HybridAllocator.sol';
+import {BATCH_COMPACT_WITNESS_TYPEHASH, MANDATE_TYPEHASH} from 'src/allocators/lib/TypeHashes.sol';
+
+import {IOriginSettler} from 'src/interfaces/ERC7683/IOriginSettler.sol';
+
+contract HybridERC7683 is HybridAllocator, IOriginSettler {
+    // keccak256("OrderData(address arbiter,address sponsor,uint256 expires,uint256[2][] idsAndAmounts,
+    //          uint256 chainId,address tribunal,address recipient,address settlementToken,uint256 minimumAmount,uint256 baselinePriorityFee,uint256 scalingFactor,uint256[] decayCurve,bytes32 salt,uint256 targetBlock,uint256 maximumBlocksAfterTarget)")
+    bytes32 constant ORDERDATA_TYPEHASH = 0xf93147c220566c5d99eedaa4ddd899c0a796b3721e418131b49cc9eedde5054d;
+
+    struct OrderData {
+        // BATCH COMPACT
+        address arbiter; // The account tasked with verifying and submitting the claim.
+        address sponsor; // The account to source the tokens from.
+        // uint256 nonce; // A parameter to enforce replay protection, scoped to allocator.
+        uint256 expires; // The time at which the claim expires.
+        uint256[2][] idsAndAmounts; // The ids of the ERC6909 tokens to allocate.
+        // MANDATE
+        uint256 chainId; // (implicit arg, included in EIP712 payload)
+        address tribunal; // (implicit arg, included in EIP712 payload)
+        address recipient; // Recipient of settled tokens
+        // uint256 expires; // Mandate expiration timestamp
+        address settlementToken; // Settlement token (address(0) for native)
+        uint256 minimumAmount; // Minimum settlement amount
+        uint256 baselinePriorityFee; // Base fee threshold where scaling kicks in
+        uint256 scalingFactor; // Fee scaling multiplier (1e18 baseline)
+        uint256[] decayCurve; // Block durations, fill increases, & claim decreases.
+        bytes32 salt; // Replay protection parameter
+        // ADDITIONAL INPUT
+        uint256 targetBlock; // The block number at the target chain on which the PGA is executed / the reverse dutch auction starts.
+        uint256 maximumBlocksAfterTarget; // Blocks after target block that are still fillable.
+    }
+
+    error InvalidOrderDataType(bytes32 orderDataType, bytes32 expectedOrderDataType);
+    error InvalidOriginSettler(address originSettler, address expectedOriginSettler);
+
+    constructor(address compact_, address signer_) HybridAllocator(compact_, signer_) {}
+
+    function openFor(
+        GaslessCrossChainOrder calldata, /*order_*/
+        bytes calldata, /*sponsorSignature_*/
+        bytes calldata /*originFillerData*/
+    ) external pure {
+        revert Unsupported();
+    }
+
+    function open(OnchainCrossChainOrder calldata order_) external {
+        // Check if orderDataType is the one expected by the allocator
+        if (order_.orderDataType != ORDERDATA_TYPEHASH) {
+            revert InvalidOrderDataType(order_.orderDataType, ORDERDATA_TYPEHASH);
+        }
+
+        OrderData calldata orderData = _decodeOrderData(order_);
+
+        // create witness hash
+        bytes32 witnessHash = keccak256(
+            abi.encode(
+                MANDATE_TYPEHASH,
+                orderData.chainId,
+                orderData.tribunal,
+                orderData.recipient,
+                order_.fillDeadline,
+                orderData.settlementToken,
+                orderData.minimumAmount,
+                orderData.baselinePriorityFee,
+                orderData.scalingFactor,
+                keccak256(abi.encodePacked(orderData.decayCurve)),
+                orderData.salt
+            )
+        );
+
+        // register claim
+        (, uint256[] memory registeredAmounts, uint256 nonce_) = registerClaim(
+            orderData.sponsor,
+            orderData.idsAndAmounts,
+            orderData.arbiter,
+            orderData.expires,
+            BATCH_COMPACT_WITNESS_TYPEHASH,
+            witnessHash
+        );
+        Lock[] memory locks = new Lock[](registeredAmounts.length);
+        for (uint256 i = 0; i < registeredAmounts.length; i++) {
+            locks[i] = _createLock(orderData.idsAndAmounts[i][0], registeredAmounts[i]);
+        }
+
+        BatchCompact memory batchCompact = BatchCompact({
+            arbiter: orderData.arbiter,
+            sponsor: orderData.sponsor,
+            nonce: nonce_,
+            expires: orderData.expires,
+            commitments: locks
+        });
+
+        // emit open event
+        emit Open(
+            bytes32(batchCompact.nonce), _convertToResolvedCrossChainOrder(orderData, order_.fillDeadline, batchCompact)
+        );
+    }
+
+    function resolveFor(GaslessCrossChainOrder calldata, /*order*/ bytes calldata /*originFillerData*/ )
+        external
+        pure
+        returns (ResolvedCrossChainOrder memory)
+    {
+        revert Unsupported();
+    }
+
+    function resolve(OnchainCrossChainOrder calldata order) external view returns (ResolvedCrossChainOrder memory) {
+        OrderData calldata orderData = _decodeOrderData(order);
+        uint256 idsLength = orderData.idsAndAmounts.length;
+        Lock[] memory locks = new Lock[](idsLength);
+        for (uint256 i = 0; i < idsLength; i++) {
+            uint256 id = orderData.idsAndAmounts[i][0];
+            locks[i] = _createLock(id, orderData.idsAndAmounts[i][1]);
+        }
+        BatchCompact memory batchCompact = BatchCompact({
+            arbiter: orderData.arbiter,
+            sponsor: orderData.sponsor,
+            nonce: nonce + 1, // nonce is incremented by 1 when the claim is registered
+            expires: orderData.expires,
+            commitments: locks
+        });
+        return _convertToResolvedCrossChainOrder(orderData, order.fillDeadline, batchCompact);
+    }
+
+    /**
+     * OnChainCrossChainOrder
+     * - 1 word for fillDeadline
+     * - 1 word for orderDataType
+     * - 1 word for orderData offset
+     */
+    function _decodeOrderData(OnchainCrossChainOrder calldata order_)
+        internal
+        pure
+        returns (OrderData calldata orderData)
+    {
+        // bytes calldata rawOrderData = LibBytes.dynamicStructInCalldata(order_.orderData, 0x40);
+        assembly ("memory-safe") {
+            orderData := order_
+        }
+    }
+
+    function _convertToResolvedCrossChainOrder(
+        OrderData calldata orderData,
+        uint256 fillDeadline,
+        BatchCompact memory batchCompact
+    ) internal view returns (ResolvedCrossChainOrder memory) {
+        Output[] memory maxSpent = new Output[](1);
+        maxSpent[0] = Output({
+            token: bytes32(uint256(uint160(orderData.settlementToken))),
+            amount: type(uint256).max,
+            recipient: bytes32(uint256(uint160(orderData.recipient))),
+            chainId: orderData.chainId
+        });
+
+        uint256 idsLength = orderData.idsAndAmounts.length;
+        Output[] memory minReceived = new Output[](idsLength);
+        for (uint256 i = 0; i < idsLength; i++) {
+            minReceived[i] = Output({
+                token: bytes32(uint256(uint160(orderData.idsAndAmounts[i][0]))),
+                amount: orderData.minimumAmount,
+                recipient: _convertAddressToBytes32(orderData.recipient),
+                chainId: block.chainid
+            });
+        }
+
+        Mandate memory mandate = Mandate({
+            recipient: orderData.recipient,
+            expires: fillDeadline,
+            token: orderData.settlementToken,
+            minimumAmount: orderData.minimumAmount,
+            baselinePriorityFee: orderData.baselinePriorityFee,
+            scalingFactor: orderData.scalingFactor,
+            decayCurve: orderData.decayCurve,
+            salt: orderData.salt
+        });
+        BatchClaim memory claim = BatchClaim({
+            chainId: block.chainid,
+            compact: batchCompact,
+            sponsorSignature: '', // No signature required from the sponsor, the claim will be verified via the on chain registration.
+            allocatorSignature: '' // No signature required from this allocator, it will verify the claim on chain via ERC1271.
+        });
+
+        FillInstruction[] memory fillInstructions = new FillInstruction[](1);
+        fillInstructions[0] = FillInstruction({
+            destinationChainId: orderData.chainId,
+            destinationSettler: _convertAddressToBytes32(orderData.tribunal),
+            originData: abi.encode(claim, mandate, orderData.targetBlock, orderData.maximumBlocksAfterTarget)
+        });
+
+        return ResolvedCrossChainOrder({
+            user: orderData.sponsor,
+            originChainId: block.chainid,
+            openDeadline: uint32(fillDeadline),
+            fillDeadline: uint32(fillDeadline),
+            orderId: bytes32(batchCompact.nonce),
+            maxSpent: maxSpent,
+            minReceived: minReceived,
+            fillInstructions: fillInstructions
+        });
+    }
+
+    function _convertAddressToBytes32(address address_) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(address_)));
+    }
+}
