e07c2294f39136e29e885ddd7f36027679f5af63
mgretzke <m.gretzke@vimage.de>
2025-07-23 13:22:58 +0200

    ERC7683Allocated using OnChainAllocator

--- src/interfaces/IERC7683Allocator.sol
+++ src/interfaces/IERC7683Allocator.sol
@@ -4,42 +4,33 @@ pragma solidity ^0.8.27;
 
 import {IOriginSettler} from './ERC7683/IOriginSettler.sol';
 import {IAllocator} from '@uniswap/the-compact/interfaces/IAllocator.sol';
+import {Lock} from '@uniswap/the-compact/types/EIP712Types.sol';
 
 interface IERC7683Allocator is IOriginSettler, IAllocator {
-    struct OrderData {
-        // COMPACT
+    struct OrderDataOnChain {
+        // BATCH_COMPACT
         address arbiter; // The account tasked with verifying and submitting the claim.
-        address sponsor; // The account to source the tokens from.
-        uint256 nonce; // A parameter to enforce replay protection, scoped to allocator.
+        // address sponsor; // The account to source the tokens from.
+        // uint256 nonce; // A parameter to enforce replay protection, scoped to allocator.
         uint256 expires; // The time at which the claim expires.
-        bytes12 lockTag; // The token ID of the ERC6909 token to allocate.
-        address inputToken; // The token address of the ERC6909 token to allocate.
-        uint256 amount; // The amount of ERC6909 tokens to allocate.
-        // MANDATE
-        uint256 chainId; // (implicit arg, included in EIP712 payload)
-        address tribunal; // (implicit arg, included in EIP712 payload)
-        address recipient; // Recipient of settled tokens
-        // uint256 expires; // Mandate expiration timestamp
-        address settlementToken; // Settlement token (address(0) for native)
-        uint256 minimumAmount; // Minimum settlement amount
-        uint256 baselinePriorityFee; // Base fee threshold where scaling kicks in
-        uint256 scalingFactor; // Fee scaling multiplier (1e18 baseline)
-        uint256[] decayCurve; // Block durations, fill increases, & claim decreases.
-        bytes32 salt; // Replay protection parameter
+        Order order; // The remaining BatchCompact and Mandate data
         // ADDITIONAL INPUT
-        uint256 targetBlock; // The block number at the target chain on which the PGA is executed / the reverse dutch auction starts.
-        uint256 maximumBlocksAfterTarget; // Blocks after target block that are still fillable.
+        uint200 targetBlock; // The block number at the target chain on which the PGA is executed / the reverse dutch auction starts.
+        uint56 maximumBlocksAfterTarget; // Blocks after target block that are still fillable.
     }
 
     struct OrderDataGasless {
-        // COMPACT
+        // BATCH_COMPACT
         address arbiter; // The account tasked with verifying and submitting the claim.
         // address sponsor; // The account to source the tokens from.
         // uint256 nonce; // A parameter to enforce replay protection, scoped to allocator.
         // uint256 expires; // The time at which the claim expires.
-        bytes12 lockTag; // The lock tag of the ERC6909 token to allocate.
-        address inputToken; // The token address of the ERC6909 token to allocate.
-        uint256 amount; // The amount of ERC6909 tokens to allocate.
+        Order order; // The remaining BatchCompact and Mandate data
+    }
+
+    /// @dev The data that OnChain and Gasless orders have in common
+    struct Order {
+        Lock[] commitments; // The token IDs and amounts to allocate.
         // MANDATE
         uint256 chainId; // (implicit arg, included in EIP712 payload)
         address tribunal; // (implicit arg, included in EIP712 payload)
@@ -55,10 +46,9 @@ interface IERC7683Allocator is IOriginSettler, IAllocator {
 
     error InvalidOriginSettler(address originSettler, address expectedOriginSettler);
     error InvalidOrderDataType(bytes32 orderDataType, bytes32 expectedOrderDataType);
-    error InvalidNonce(uint256 nonce);
-    error InvalidSignature(address signer, address expectedSigner);
-    error InvalidRegistration(address sponsor, bytes32 claimHash);
+    error InvalidNonce(uint256 nonce, uint256 expectedNonce);
     error BatchCompactsNotSupported();
+    error InvalidAllocatorData(bytes32 expectedAllocatorData, bytes32 actualAllocatorData);
 
     /// @notice Returns the type string of the compact including the witness
     function getCompactWitnessTypeString() external pure returns (string memory);
